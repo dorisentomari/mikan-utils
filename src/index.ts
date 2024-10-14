5a976e97705993f9b844b3057488775eecfd845e
dorisentomari <dorisentomari@gmail.com>
2024-10-14 21:05:20 +0800

    删除旧的代码

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import * as array from './array';
 import * as browser from './browser';
 import * as common from './common';
-import * as csv from './csv';
-import * as date from './date';
 import * as number from './number';
 import * as object from './object';
 import * as random from './random';
@@ -13,8 +11,6 @@ export default {
   ...array,
   ...browser,
   ...common,
-  ...csv,
-  ...date,
   ...number,
   ...object,
   ...random,
